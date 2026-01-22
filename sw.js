1cd112caea97a95a076a5f7174acf267adf1bf5f
Robertas <4360582+monkroe@users.noreply.github.com>
2026-01-22 11:03:31 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'robert-os-v1.1';
+const CACHE_NAME = 'robert-os-v1.1-safe';
 const ASSETS = [
   '/',
   '/index.html',
@@ -9,13 +9,31 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); 
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
+  // STRATEGIJA: Network First (Saugiausia)
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });

7e09aa65b0224005bc6329b976707f918f8c146e
Robertas <4360582+monkroe@users.noreply.github.com>
2026-01-22 10:55:16 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'robert-os-v1';
+const CACHE_NAME = 'robert-os-v1.1';
 const ASSETS = [
   '/',
   '/index.html',
@@ -8,16 +8,14 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
-// Instaliavimas: Išsaugome failus
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Veikimas: Pirmiausia rodome iš Cache (greitis), tada siunčiame užklausą
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });

91957a6a54ba9880824bf75db49067ac36144161
Robertas <4360582+monkroe@users.noreply.github.com>
2026-01-22 12:20:20 -0600

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,1 +1,7 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://sopcisskptiqlllehhgb.supabase.co', // <--- ĮRAŠYK
+    SUPABASE_KEY: 'sb_publishable_AqLNLewSuOEcbOVUFuUF-A_IWm9L6qy'  // <--- ĮRAŠYK
+};
+
+export const db = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
 

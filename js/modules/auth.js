f7b530edb4990bd03f946631abc39ae32bf419df
Robertas <4360582+monkroe@users.noreply.github.com>
2026-01-22 12:23:18 -0600

    Update auth.js

--- js/modules/auth.js
+++ js/modules/auth.js
@@ -1,1 +1,25 @@
+import { db } from '../db.js';
+import { showToast, vibrate } from '../utils.js';
+
+export async function login() {
+    vibrate();
+    const email = document.getElementById('auth-email').value;
+    const password = document.getElementById('auth-pass').value;
+    
+    const { error } = await db.auth.signInWithPassword({email, password});
+    
+    if(error) showToast(error.message, 'error'); 
+    else location.reload();
+}
+
+export async function logout() {
+    vibrate();
+    const savedTheme = localStorage.getItem('theme');
+    
+    await db.auth.signOut();
+    localStorage.clear();
+    
+    if (savedTheme) localStorage.setItem('theme', savedTheme);
+    location.reload();
+}
 

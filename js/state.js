ce8dd8ae2f7a8c348b2559b0eef69b5b9431c086
Robertas <4360582+monkroe@users.noreply.github.com>
2026-01-22 23:39:18 -0600

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,18 +1,28 @@
-// --- STATE.JS (BE CIKLŲ) ---
+// --- STATE.JS (v1.1.1 - FULL UPDATE) ---
 
 export const state = new Proxy({
+    // Esami kintamieji
     user: null,
     fleet: [],
     activeShift: null,
     dailyCost: 0,
     shiftEarnings: 0,
     txDirection: 'in',
-    loading: false
+    loading: false,
+
+    // Nauji kintamieji išmaniam laikmačiui ir tikslams
+    pausedAtTime: null,    // Saugosime laiką tekstiniu formatu (pvz., "14:20"), kada paspausta pauzė
+    lastDrivingMs: 0,      // Sukauptas gryno vairavimo laikas milisekundėmis iki pauzės
+    
+    targetMoney: 0,        // Piniginis tikslas pamainai
+    targetTime: 12,        // Laiko tikslas valandomis (default 12 val.)
+    
+    currentWeather: 'sunny' // Numatytosios vairavimo sąlygos
 }, {
     set(target, key, value) {
         target[key] = value;
         
-        // Vietoj tiesioginio kvietimo, siunčiame signalą į eterį
+        // Siunčiame signalą, kad būsena pasikeitė
         window.dispatchEvent(new CustomEvent('state-updated', { detail: key }));
         
         return true;
